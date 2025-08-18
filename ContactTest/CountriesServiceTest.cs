52a12b2a9eacfc8219dacf7d5e1e50e4e12aecb8
MRLIGHT02 <saifali42020@gmail.com>
2025-08-18 14:14:52 +0530

    Adding the Country service and test to the project

---
+++ ContactTest/CountriesServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using ServiceContracts;
+using Entities;
+using System;
+using System.Collections.Generic;
+using Services;
+
+
+namespace ContactTest
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+        
+        public CountriesServiceTest()
+        {
+            _countriesService =new CountriesService();
+
+        }
+
+    }
+}
---
+++ ServiceContracts/ICountriesService.cs
@@ -0,0 +1,18 @@
+﻿using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represent business Logic for manipulating Country entity
+    /// </summary>
+    public interface ICountriesService
+    {
+        
+        /// <summary>
+        /// Add a country object to the list of countries
+        /// </summary>
+        /// <param name="countryAddRequest">Country object to add </param>
+        /// <returns>Returns the Country object after adding it (including newly generated country id)</returns>
+        CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
+    }
+}
