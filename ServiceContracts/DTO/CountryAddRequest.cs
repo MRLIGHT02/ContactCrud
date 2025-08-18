4ae13b7fd595e0e7af28afebdc837f93ab965fab
MRLIGHT02 <saifali42020@gmail.com>
2025-08-18 11:37:02 +0530

    Add ServiceContracts project with DTOs and interface
    - Introduced a new project "ServiceContracts" in the solution.
    - Created the `ICountryService` interface for country-related services.
    - Added `ServiceContracts.csproj` with SDK and project reference to `Entities`.
    - Implemented `CountryAddRequest` DTO for adding new countries.
    - Added `CountryResponse` DTO and `CountryExtensions` for response handling.

---
+++ ServiceContracts/DTO/CountryAddRequest.cs
@@ -0,0 +1,26 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class to represents a request to add a new country.
+    /// </summary>
+    /// <remarks>This class is used to encapsulate the data required to create a new country entry.</remarks>
+    public class CountryAddRequest
+    {
+        public string? CountryName { get; set; }
+
+        public Country ToCountry()
+        {
+            return new Country() { CountryName = this.CountryName };
+        }
+
+        
+
+    }
+}
---
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,39 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// Represents a response containing information about a country.
+    /// </summary>
+    /// <remarks>This class is typically used to encapsulate country-related data in API responses.</remarks>
+    public class CountryResponse
+    {
+        public Guid CountryId { get; set; }
+        public string? CountryName { get; set; }
+
+
+
+
+    }
+    /// <summary>
+    /// Provides extension methods for the <see cref="Country"/> type.
+    /// </summary>
+    /// <remarks>This class contains methods that extend the functionality of the <see cref="Country"/> type,
+    /// enabling additional operations such as converting a <see cref="Country"/> instance to a <see
+    /// cref="CountryResponse"/>.</remarks>
+    public static class CountryExtensions {
+        
+        public static CountryResponse ToCountryResponse(this Country country)
+        {
+            return new CountryResponse() { CountryName=country.CountryName,CountryId=country.CountryId };
+
+        }
+
+    }
+
+}
